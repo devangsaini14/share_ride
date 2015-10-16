94c1e3490de62c5a41707449df4bec952208287f
rishabhgupta <rishabhgupta@msn.com>
2015-10-16 16:32:50 +0530

    Adds requires for gmap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,9 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require bootstrap-sprockets
+//= require underscore
+//= require gmaps/google
 //= require_tree .
