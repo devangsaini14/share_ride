902d780b6b3c27e15b3fb3bc59ad4654bbe06de7
devangsaini14 <devang.saini@aspiresys.com>
2015-10-16 13:38:25 +0530

    Added create and update to rides api

--- app/controllers/api/v1/rides_controller.rb
+++ app/controllers/api/v1/rides_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::RidesController < ApplicationController
+class Api::V1::RidesController < BaseApiController
   before_filter :set_ride, only: [:show, :update, :destroy]
 
   def index
@@ -11,21 +11,21 @@ class Api::V1::RidesController < ApplicationController
   end
 
   def create
-    @ride = Ride.new(params[:ride])
-
+    @ride = Ride.new
+    @ride.assign_attributes(@json['ride'])
     if @ride.save
-      render json: @ride, status: :created, location: @ride
+      render nothing: true, status: 200
     else
-      render json: @ride.errors, status: :unprocessable_entity
+      render nothing: true, status: :bad_request
     end
   end
 
   def update
-    
-    if @ride.update(params[:ride])
-      head :no_content
+    @ride.assign_attributes(@json['ride'])
+    if @ride.save
+        render json: @ride, status: 200
     else
-      render json: @ride.errors, status: :unprocessable_entity
+        render nothing: true, status: :bad_request
     end
   end
 
@@ -34,29 +34,11 @@ class Api::V1::RidesController < ApplicationController
     head :no_content
   end
 
-  # # def filter_fields
-  # # 	@filter_values = {}
-  # #   @filter_values[:request_category_id] = params[:category_id]
-  # #   @filter_values[:filters] = HomeProperty.find_filter_values(params)
-  # #   @filter_values[:home_types] = HomeType.all
-  # #   @filter_values[:builderslist] = builder_data
-  # #   render json: @filter_values
-  # # end
-
-  # def builder_data
-  #   @builders = {}
-  #   @builders[:count] = Builder.all.count
-  #   @builders[:items] = Builder.all
-  #   @builders
-  # end
-
+ 
   private
 
     def set_ride
       @ride = Ride.find(params[:id])
     end
 
-    # def ride_params
-    #   params.require(:ride).permit(:source :source_latitude :source_longitude :destination :destination_latitude :destination_longitude :price :date :seats :contact :email)
-    # end
 end
